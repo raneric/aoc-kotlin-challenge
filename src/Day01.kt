2e7c14e3c465bf31f42839894320ebf20199ace3
Eric RANDRIANARISOA <raneric@gmail.com>
2022-12-02 20:56:27 +0300

    refactoring DAY 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -15,27 +15,19 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        var first = Int.MIN_VALUE
-        var second = Int.MIN_VALUE
-        var third = Int.MIN_VALUE
-
+        val sum = ArrayList<Int>()
         var tempData = 0
 
         input.map {
             if (it.isNotEmpty()) {
                 tempData += it.toInt()
             } else {
-                if (first < tempData) {
-                    first = tempData
-                } else if (second < tempData) {
-                    second = tempData
-                } else if (third < tempData) {
-                    third = tempData
-                }
+                sum.add(tempData)
                 tempData = 0
             }
         }
-        return first + second + third
+        sum.sortDescending()
+        return sum.take(3).sum()
     }
 
     val input = readInput("Day01")
